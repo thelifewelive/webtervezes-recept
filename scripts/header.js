d7eed9b24a63fb11285f82794687f880364615e7
Kovács Ádám <h663315@stud.u-szeged.hu>
2019-03-10 20:02:41 +0100

    alap design

---
+++ scripts/header.js
@@ -0,0 +1,27 @@
+function setHeaderImage(){
+	var random = Math.floor(Math.random() * 7);
+	console.log(random);
+	switch(random){
+		case 0:
+			document.getElementById('header').style.backgroundImage = "url('images/back1.jpg')";
+			break;
+		case 1:
+			document.getElementById('header').style.backgroundImage = "url('images/back2.jpg')";
+			break;
+		case 2:
+			document.getElementById('header').style.backgroundImage = "url('images/back3.jpg')";
+			break;
+		case 3:
+			document.getElementById('header').style.backgroundImage = "url('images/back4.jpg')";
+			break;
+		case 4:
+			document.getElementById('header').style.backgroundImage = "url('images/back5.jpg')";
+			break;
+		case 5:
+			document.getElementById('header').style.backgroundImage = "url('images/back6.jpg')";
+			break;
+		case 6:
+			document.getElementById('header').style.backgroundImage = "url('images/back7.jpg')";
+			break;
+	}
+}
